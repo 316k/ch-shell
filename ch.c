654064c1a84c2bc8e61a75eaed60632b3952e49a
Nicolas Hurtubise <316k@legtux.org>
2016-01-18 16:12:33 -0500

    First commit

---
+++ ch.c
@@ -0,0 +1,15 @@
+/* ch.c --- Un shell pour les hélvètes.  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (void)
+{
+	fprintf (stdout, "%% ");
+	
+	/* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
+	
+	fprintf (stdout, "Bye!\n");
+	exit (0);
+}
